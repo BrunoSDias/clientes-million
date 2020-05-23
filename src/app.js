ff38ddd92b119f6df55625c2b82ed4cdc35559d0
BrunoSDias <brunosdias@outlook.com>
2020-05-23 15:42:54 -0300

    DB Env Var Config, Cross Domain

--- src/app.js
+++ src/app.js
@@ -5,4 +5,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/',require('./routes'))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", '*');
+  res.header("Access-Control-Request-Method", "*");
+  res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, application/json, Content-Type, Accept, token")
+})
+
 module.exports = app
